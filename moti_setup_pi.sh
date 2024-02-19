dcbe13b2a87b2035b64c0408e7f7912213b04b41
Moti Elitzur <42735869+MotiElitzur@users.noreply.github.com>
2024-02-19 11:47:08 +0200

    Add my setup

--- moti_setup_pi.sh
+++ moti_setup_pi.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 # Variables
-# Ask the user for Raspberry Pi username
-echo "Please enter the Raspberry Pi username:"
-read RASPBERRY_PI_USER
-
+RASPBERRY_PI_USER='moti' # Make sure to use straight quotes here
 RASPBERRY_PI_IP="$RASPBERRY_PI_USER.local"
 
 FOLDER_NAME='BeaconSimulator'
@@ -18,7 +15,7 @@ PATH_TO_PYTHON_SCRIPT="$PATH_TO_FOLDER/$PYTHON_FOLDER_NAME/HttpServer.py"
 chmod -R +x "."
 
 # Delete the target directory on the Raspberry Pi
-sshpass -p "$RASPBERRY_PI_USER" ssh "$RASPBERRY_PI_USER@$RASPBERRY_PI_IP" "rm -rf $TARGET_PATH_ON_PI/$FOLDER_NAME"
+# sshpass -p "$RASPBERRY_PI_USER" ssh "$RASPBERRY_PI_USER@$RASPBERRY_PI_IP" "rm -rf $TARGET_PATH_ON_PI/$FOLDER_NAME"
 
 # Copy the entire BeaconSimulator (current) folder to Raspberry Pi, excluding hidden files and the venv folder
 sshpass -p "$RASPBERRY_PI_USER" rsync -avz --delete \
@@ -34,6 +31,10 @@ sshpass -p "$RASPBERRY_PI_USER" ssh -o StrictHostKeyChecking=no "$RASPBERRY_PI_U
 # Update the raspberry pi, and remove unnecessary packages if any
 sudo apt update && sudo apt upgrade && sudo apt autoremove
 
+# Configure the Bluetooth adapter to be discoverable
+# sudo hciconfig hci0 up
+#sudo hciconfig hci0 leadv 3
+
 # Navigate to the project directory
 cd "$PATH_TO_FOLDER"
 
@@ -43,6 +44,8 @@ chmod -R +x "./"
 # Terminate any currently running instances of the Python script
 #killall -9 $PYTHON_SCRIPT_NAME
 
+# rm -f $PATH_TO_FOLDER/database.db
+
 # Create a Python virtual environment
 python3 -m venv venv
 
@@ -50,10 +53,10 @@ python3 -m venv venv
 source venv/bin/activate
 
 # Upgrade pip itself
-pip install --upgrade pip
+# pip install --upgrade pip
 
 # Install dependencies from requirements.txt              TODO: Uncomment this line if its already installed because it will take a long time to install
-pip install -r requirements.txt
+# pip install -r requirements.txt
 
 #clear the old cron jobs
 crontab -r
@@ -63,5 +66,8 @@ echo "@reboot python3 $PATH_TO_PYTHON_SCRIPT >> $PATH_TO_FOLDER/$FOLDER_NAME.log
 
 # List the cron jobs
 crontab -l
+
+# Run the Python script immediately and redirect output to the log file
+# nohup python3 $PATH_TO_PYTHON_SCRIPT >> $PATH_TO_FOLDER/$FOLDER_NAME.log 2>&1 &
 
 EOF
